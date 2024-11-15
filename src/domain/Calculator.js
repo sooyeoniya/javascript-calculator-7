5afb37b1d51048fe348c2289a5bc4f8c8cfadfea
sooyeoniya <tndus1306403@naver.com>
2024-11-15 19:39:34 +0900

    feat(Calculator): 커스텀 구분자 존재하지 않는 경우 및 입력 형식 에러 처리

--- src/domain/Calculator.js
+++ src/domain/Calculator.js
@@ -1,56 +1,63 @@
 import OutputView from '../view/OutputView.js';
 
+const INPUT_REGEX = /^(\/\/(?<customDelimiters>.*)\\n)?(?<expression>.*)$/;
+
 class Calculator {
-  #input = '';
   #delimiter = ',:';
-  #parsedInput = '';
+  #customDelimiters;
+  #expression;
 
   constructor(input) {
-    this.#input = input;
-    this.#parsedInput = input;
+    this.#extractCustomDelimitersAndExpression(input);
+    this.#validation(input);
   }
 
-  validate() {
-    this.#validateStringFormat();
+  #extractCustomDelimitersAndExpression(input) {
+    const { customDelimiters, expression } = input.match(INPUT_REGEX).groups;
+    this.#customDelimiters = customDelimiters;
+    this.#expression = expression;
+    console.log(this.#customDelimiters, this.#expression);
   }
 
-  calculate() {
-    if (this.#input.startsWith('//')) {
-      this.#extractInputString();
-      this.#extractDelimiters();
+  #validation(input) {
+    if (!this.#customDelimiters && (this.#expression.includes('//') || this.#expression.includes('\\n'))) {
+      OutputView.printErrorMessage('입력 형식이 올바르지 않습니다. 다시 입력해주세요.');
     }
-    return this.#addNumbers();
-  }
-
-  #extractInputString() {
-    const splitInput = this.#input.split('\\n');
-    this.#parsedInput = splitInput[splitInput.length - 1];
-    console.log('this.#parsedInput: ' + this.#parsedInput);
-  }
-
-  #extractDelimiters() {
-    const customDelimiters = this.#input.split('//')[1].split('\\n')[0];
-    console.log('customDelimiters: ' + customDelimiters);
-    this.#delimiter += customDelimiters;
-    console.log('this.#delimiter: ' + this.#delimiter);
-  }
-
-  #addNumbers() {
-    const regex = new RegExp(`[${this.#delimiter}]`)
-    const parsedNumbers = this.#parsedInput.split(regex).map(number => number.trim());
-    console.log('parsedNumbers');
-    console.log(parsedNumbers);
-    const result = parsedNumbers.reduce((acc, cur) => acc + Number(cur), 0);
-    console.log('result: ' + result);
-    return result;
-  }
-
-  #validateStringFormat() {
-    const regex = /^(\/\/(?<customDelimiters>)\\n)?(?<expression>.*)$/;
-    if (!regex.test(this.#input)) {
-      OutputView.printErrorMessage('올바르지 않은 형식으로 입력했습니다. 다시 입력해주세요.');
+    if (!this.#customDelimiters && input.startsWith('//')) {
+      OutputView.printErrorMessage('커스텀 구분자가 존재하지 않습니다. 다시 입력해주세요.');
     }
   }
+
+  // calculate() {
+  //   if (this.#input.startsWith('//')) {
+  //     this.#extractInputString();
+  //     this.#extractDelimiters();
+  //   }
+  //   return this.#addNumbers();
+  // }
+
+  // #extractInputString() {
+  //   const splitInput = this.#input.split('\\n');
+  //   this.#parsedInput = splitInput[splitInput.length - 1];
+  //   console.log('this.#parsedInput: ' + this.#parsedInput);
+  // }
+
+  // #extractDelimiters() {
+  //   const customDelimiters = this.#input.split('//')[1].split('\\n')[0];
+  //   console.log('customDelimiters: ' + customDelimiters);
+  //   this.#delimiter += customDelimiters;
+  //   console.log('this.#delimiter: ' + this.#delimiter);
+  // }
+
+  // #addNumbers() {
+  //   const regex = new RegExp(`[${this.#delimiter}]`)
+  //   const parsedNumbers = this.#parsedInput.split(regex).map(number => number.trim());
+  //   console.log('parsedNumbers');
+  //   console.log(parsedNumbers);
+  //   const result = parsedNumbers.reduce((acc, cur) => acc + Number(cur), 0);
+  //   console.log('result: ' + result);
+  //   return result;
+  // }
 }
 
 export default Calculator;
