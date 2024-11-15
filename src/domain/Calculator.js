b15a9d4bebac54954c42f193c115fac0de2e0824
sooyeoniya <tndus1306403@naver.com>
2024-11-15 22:50:26 +0900

    refactor(Calculator): validation 함수 리팩토링
    - 함수 길이 10 이하로 줄이기
    - 각 오류 로직별 함수 분리

--- src/domain/Calculator.js
+++ src/domain/Calculator.js
@@ -28,23 +28,36 @@ class Calculator {
   }
 
   #validation(input, customDelimiters, expression, parsedNumbers) {
+    this.#validateInputForm(customDelimiters, expression);
+    this.#validateNoCustomDelimiter(input, customDelimiters);
+    this.#validateNegativeNum(customDelimiters, expression);
+    this.#validateDefinedDelimiters(parsedNumbers);
+  }
+
+  #validateInputForm(customDelimiters, expression) {
     if (!customDelimiters 
       && (expression.includes(CUSTOM_DELIMITERS.START) 
       || expression.includes(CUSTOM_DELIMITERS.END))) {
       OutputView.printErrorMessage(ERROR_MESSAGES.INPUT_FORM);
     }
+  }
 
+  #validateNoCustomDelimiter(input, customDelimiters) {
     if (!customDelimiters && input.startsWith(CUSTOM_DELIMITERS.START)) {
       OutputView.printErrorMessage(ERROR_MESSAGES.NO_CUSTOM_DELIMITERS);
     }
+  }
 
+  #validateNegativeNum(customDelimiters, expression) {
     if (expression.includes(NEGATIVE_NUM)
       && (!customDelimiters
       || (customDelimiters && !customDelimiters.includes(NEGATIVE_NUM)))
     ) {
       OutputView.printErrorMessage(ERROR_MESSAGES.NEGATIVE_NUM);
     }
+  }
 
+  #validateDefinedDelimiters(parsedNumbers) {
     if (parsedNumbers.some((delimiter) => isNaN(Number(delimiter)))) {
       OutputView.printErrorMessage(ERROR_MESSAGES.NO_DEFINITION_DELIMITERS);
     }
